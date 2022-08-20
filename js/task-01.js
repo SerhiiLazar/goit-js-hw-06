af996d6154ee877c6dd187d17d367820941ad756
Serhii Lazarenok <serhiilazar90@gmail.com>
2022-08-20 23:31:57 +0300

    Task 1

---
+++ js/task-01.js
@@ -0,0 +1,57 @@
+// Задание 1
+// В HTML есть список категорий ul#categories.
+
+// <ul id="categories">
+//   <li class="item">
+//     <h2>Animals</h2>
+//     <ul>
+//       <li>Cat</li>
+//       <li>Hamster</li>
+//       <li>Horse</li>
+//       <li>Parrot</li>
+//     </ul>
+//   </li>
+//   <li class="item">
+//     <h2>Products</h2>
+//     <ul>
+//       <li>Bread</li>
+//       <li>Prasley</li>
+//       <li>Cheese</li>
+//     </ul>
+//   </li>
+//   <li class="item">
+//     <h2>Technologies</h2>
+//     <ul>
+//       <li>HTML</li>
+//       <li>CSS</li>
+//       <li>JavaScript</li>
+//       <li>React</li>
+//       <li>Node.js</li>
+//     </ul>
+//   </li>
+// </ul>
+
+// Напиши скрипт который:
+
+// Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
+// Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега <h2>) и количество элементов в категории (всех вложенных в него <li>).
+// В результате, в консоли будут выведены такие сообщения.
+
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
+const navItemEl = document.querySelectorAll('.item');
+console.log('Number of categories:', navItemEl.length );
+
+navItemEl.forEach(categories => {
+
+    console.log('\nCategory:', categories.firstElementChild.textContent);
+    console.log('Elements:', categories.lastElementChild.children.length);
+})
