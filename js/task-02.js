a6a14efc08252c1ce57455959cb6b8768c87230d
Serhii Lazarenok <serhiilazar90@gmail.com>
2022-08-21 12:30:40 +0300

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,26 @@
+// Задание 2
+// В HTML есть пустой список ul#ingredients.
+
+// <ul id="ingredients"></ul>
+
+// В JavaScript есть массив строк.
+
+// const ingredients = [
+//   "Potatoes",
+//   "Mushrooms",
+//   "Garlic",
+//   "Tomatos",
+//   "Herbs",
+//   "Condiments",
+// ];
+
+// Напиши скрипт, который для каждого элемента массива ingredients:
+
+// Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
+// Добавит название ингредиента как его текстовое содержимое.
+// Добавит элементу класс item.
+// После чего вставит все <li> за одну операцию в список ul#ingredients.
+
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +29,27 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientEl = document.querySelector('#ingredients');
+
+const el = [];
+
+for (let i=0; i<ingredients.length; i += 1){
+  const ingredint = ingredients[i];
+
+  const liEl = document.createElement('li');
+
+  liEl.textContent = ingredint;
+  liEl.classList.add('item');
+
+  el.push(liEl);
+}
+
+ingredientEl.append(...el);
+
+console.log(ingredientEl);
+  
+
+
+
+
--- js/task-03.js
+++ js/task-03.js
@@ -1,3 +1,27 @@
+// Задание 3
+// Напиши скрипт для создания галереи изображений по массиву данных. В HTML есть список ul.gallery.
+
+// <ul class="gallery"></ul>
+
+// Используй массив объектов images для создания элементов <img> вложенных в <li>. Для создания разметки используй шаблонные строки и метод insertAdjacentHTML().
+
+// Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+// Добавь минимальное оформление галереи флексбоксами или гридами через CSS классы.
+// const images = [
+//   {
+//     url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+//     alt: "White and Black Long Fur Cat",
+//   },
+//   {
+//     url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+//     alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+//   },
+//   {
+//     url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+//     alt: "Group of Horses Running",
+//   },
+// ];
+
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -12,3 +36,5 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const 
